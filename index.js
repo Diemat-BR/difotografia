a38291303dd5da088ca1fb3be67617bc8f1b8bdf
Diemat-BR <sg_ilha@hotmail.com>
2025-09-01 17:23:25 -0300

    Delete index.js
    JS desnecessário.

--- index.js
+++
@@ -1,6 +0,0 @@
-
-
-
-let mensagem = document.querySelector("#msg");
-botao.addEventListener('click', abrirTXT)
-function abrirTXT() {mensagem.innerHTML="Dê mais valor ao que você tem de melhor! Como fotógrafo de pessoas, eu busco o melhor ângulo de cada um. E isso me ensina que devemos fazer o mesmo a nível pessoal, sendo menos duro consigo mesmo as vezes.";}
